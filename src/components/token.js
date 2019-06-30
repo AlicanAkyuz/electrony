8d8785a95f15751ee1f18dbb6b166a733ec44bd7
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-06-30 14:50:51 +0300

    token log

--- src/components/token.js
+++ src/components/token.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import queryString from 'query-string';
-import { onTokenReceived, getUserData } from '../actions/selection_actions/actions';
-import { connect } from 'react-redux';
+import React from "react";
+import queryString from "query-string";
+import {
+  onTokenReceived,
+  getUserData
+} from "../actions/selection_actions/actions";
+import { connect } from "react-redux";
 
 class Token extends React.Component {
   constructor(props) {
@@ -9,29 +12,31 @@ class Token extends React.Component {
     this.updateToken = this.updateToken.bind(this);
   }
   componentDidMount() {
-    this.props.history.push('/steps')
-  };
+    this.props.history.push("/steps");
+  }
 
   updateToken(key, value) {
     localStorage.setItem(key, value);
-  };
+  }
 
   render() {
     const parsed = queryString.parse(window.location.hash);
     const accessToken = parsed.access_token;
+    console.log(accessToken);
+
     this.props.dispatch(onTokenReceived(accessToken));
     this.props.dispatch(getUserData(accessToken));
-    this.updateToken('token', accessToken);
+    this.updateToken("token", accessToken);
 
-    return (<div></div>)
-  };
-};
+    return <div />;
+  }
+}
 
 const mapStateToProps = state => {
   return {
     user_token: state.SelectionReducer.user_data.user_token,
     user_name: state.SelectionReducer.user_data.user_name,
-    user_id: state.SelectionReducer.user_data.user_id,
+    user_id: state.SelectionReducer.user_data.user_id
   };
 };
 
